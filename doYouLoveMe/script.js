c3318a59ed5a82af48fd918c92dc33ecc4f10626
Ducthien190203 <ducthien190203@gmail.com>
2025-04-16 10:41:26 +0700

    up

--- doYouLoveMe/script.js
+++
@@ -1,6 +0,0 @@
-function changePositionBtnNo() {
-    let btnNo = document.getElementById("btn-no");
-    let numberRandom = Math.random() * (500 - 100) + 100;
-    btnNo.style.left =numberRandom +'px'
-    btnNo.style.top =numberRandom +'px'
-}
